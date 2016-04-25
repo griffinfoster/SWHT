07aed1c02828d8c2abb8602425de44c506440fc4
Griffin Foster <griffin.foster@gmail.com>
2016-04-25 15:37:55 +0200

    version change to 0.1.2

--- SWHT/__init__.py
+++ SWHT/__init__.py
@@ -2,7 +2,7 @@
 A module interface to SWHT functions
 """
 
-__version__ = '0.1.1' #this needs to be kept up to date with setup.py
+__version__ = '0.1.2' #this needs to be kept up to date with setup.py
 
 import ecef, display, fileio, ft, lofarConfig, swht, util, Ylm
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import numpy as np
 import os, sys, glob
 
-__version__ = '0.1.1' #this needs to be kept up to date with SWHT/__init__.py
+__version__ = '0.1.2' #this needs to be kept up to date with SWHT/__init__.py
 
 setup(name = 'SWHT',
     version = __version__,
