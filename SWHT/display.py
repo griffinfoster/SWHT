fixed 3D plot rotation issue

SWHT/display.py
@@ -3,31 +3,60 @@ Functions to display images and coefficients
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.patches
 import numpy as np
 import swht, util
 
-def disp2D(img, dmode='abs', cmap='jet'):
+def disp2D(img, dmode='dB', cmap='jet'):
     """Display 2D projected image
     img: 2D array of complex flux values
-    dmode: string, data mode (abs, real, imaginary, phase)
+    dmode: string, data mode (abs, dB (absolute value in log units), real, imaginary, phase)
     cmap: string, matplotlib colormap name
     """
     if dmode.startswith('abs'): img = np.abs(img)
+    elif dmode.startswith('dB'): img = 10. * np.log10(np.abs(img))
     elif dmode.startswith('real'): img = img.real
     elif dmode.startswith('imag'): img = img.imag
     elif dmode.startswith('phase'): img = np.angle(img)
     else:
         print 'WARNING: Unknown data mode, defaulting to absolute value'
         img = np.abs(img)
-    
+
     img = np.fliplr(img)
 
     fig, ax = plt.subplots(1, 1)
+    ax.yaxis.set_ticks([]) # hide tick marks and labels
+    ax.xaxis.set_ticks([]) # hide tick marks and labels
+    ax.patch.set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
     plt.imshow(img, interpolation='nearest', cmap=plt.get_cmap(cmap))
+
+    # draw alt-az grid (assume square image)
+    xc = img.shape[0]/2. - 0.5
+    yc = img.shape[1]/2. - 0.5
+    ax.add_patch( matplotlib.patches.Circle((xc, yc), img.shape[0]/2., fill=False)) # alt 0
+    altLines = 5 # number of lines at constant altitude, including alt=0 and alt=90
+    deltaAlt = img.shape[0] / (2.*altLines)
+    for al in range(1, altLines): # skip alt=0 and alt=90
+        ax.add_patch( matplotlib.patches.Circle((xc, yc), xc-deltaAlt*al, fill=False, linestyle='dotted', alpha=0.7))
+    azLines = 6 # number of constant azimuth lines
+    deltaAz = np.pi / azLines
+    for az in range(azLines):
+        plt.plot(np.array([np.cos(az*deltaAz), np.cos(az*deltaAz + np.pi)])/2. + 0.5, np.array([np.sin(az*deltaAz), np.sin(az*deltaAz + np.pi)])/2. + 0.5, 'k:', alpha=0.7, transform=ax.transAxes)
+
+    # grid labels
+    for az in range(2*azLines):
+        #plt.text(img.shape[0] * (1.06 * np.cos(az*deltaAz)/2. + 0.5), img.shape[0] * (1.06 * np.sin(az*deltaAz)/2. + 0.5), '%.0f'%(az*deltaAz*180./np.pi), horizontalalignment='center')
+        plt.text(img.shape[0] * (1.06 * np.sin(az*deltaAz - np.pi)/2. + 0.5), img.shape[0] * (1.06 * np.cos(az*deltaAz - np.pi)/2. + 0.5), '%.0f'%(az*deltaAz*180./np.pi), horizontalalignment='center')
+
     plt.colorbar()
 
     return fig, ax
 
+# TODO: Add RA/Dec grid
 def disp3D(img, phi, theta, dmode='abs', cmap='jet'):
     """Display 3D, equal in phi and theta (Driscoll and Healy mapping) image
     img: 2D array of complex flux values
@@ -50,6 +79,9 @@ def disp3D(img, phi, theta, dmode='abs', cmap='jet'):
     #Z = np.sin(theta-(np.pi/2.))
     X, Y, Z = util.sph2cart(theta, phi)
 
+    # North-South Flip
+    #Z *= -1.
+
     #http://stackoverflow.com/questions/22175533/what-is-the-equivalent-of-matlabs-surfx-y-z-c-in-matplotlib
     from mpl_toolkits.mplot3d import Axes3D
     from matplotlib import cm
@@ -64,6 +96,7 @@ def disp3D(img, phi, theta, dmode='abs', cmap='jet'):
     scalarMap.set_array(img)
     C = scalarMap.to_rgba(img)
     surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, facecolors=C, antialiased=True)
+    #wire = ax.plot_wireframe(1.05*X, 1.05*Y, 1.05*Z, rstride=10, cstride=10, color='black') # RA/Dec grid
     fig.colorbar(scalarMap)
     return fig, ax
 

SWHT/fileio.py
@@ -2,7 +2,18 @@
 functions to read/write data for the imagers
 """
 
-#TODO: hdf5 wrappers for visibilities and images
+"""
+TODO: hdf5 wrappers for visibilities and images
+    visibilities:
+        LOFAR (XST or ACC) visibilities
+        UVW corrdinates
+        Flags
+        Station Information
+    images:
+        SWHT coefficients
+        Imaging history
+        Observation history
+"""
 
 import cPickle as pkl
 import numpy as np
@@ -254,8 +265,8 @@ def lofarKAIRAXST(fn, sb, nantpol, intTime, antGains=None, times='0'):
         iii) d[step size]: decimate the integrations to select an integration every 'step size', e.g. d600 will select every 600th integration
 
     returns:
-        corrMatrix: correlation matrix from each subband and integration [1, nAvgInts, nantpol, nantpol] for consistency with lofarACCSelectSbs()
-        tDeltas: 2D array [1, nAvgInts], time offset from end of file timestep, for consistency with lofarACCSelectSbs()
+        reducedCorrMatrix: correlation matrix from each subband and integration [1, tids.size, nantpol, nantpol] for consistency with lofarACCSelectSbs()
+        tDeltas: 2D array [1, tids.size], time offset from end of file timestep, for consistency with lofarACCSelectSbs()
     """
     corrMatrix = np.fromfile(fn, dtype='complex') # read in the correlation matrix
     nints = corrMatrix.shape[0]/(nantpol * nantpol) # number of integrations
@@ -282,12 +293,14 @@ def lofarKAIRAXST(fn, sb, nantpol, intTime, antGains=None, times='0'):
         reducedCorrMatrix = corrMatrix[tids]
 
     tDeltas = [] # integration timestamp deltas from the end of file
+    if antGains is not None: # Apply Gains
+        sbAntGains = antGains[sb][np.newaxis].T
+        sbVisGains = np.conjugate(np.dot(sbAntGains, sbAntGains.T)) # from Tobia, visibility gains are computed as (G . G^T)*
+        reducedCorrMatrix = sbVisGains * reducedCorrMatrix # Apply gains
     for tIdx, tid in enumerate(tids):
-        if antGains is not None: # Apply Gains
-            # TODO: these two lines are in the loop but does not need to be, only need to be called once, infact I think all 3 lines do not need a loop
-            sbAntGains = antGains[sb][np.newaxis].T
-            sbVisGains = np.conjugate(np.dot(sbAntGains, sbAntGains.T)) # from Tobia, visibility gains are computed as (G . G^T)*
-            reducedCorrMatrix[tIdx] = np.multiply(sbVisGains, reducedCorrMatrix[tid, :, :]) #select out a single integration, shape (nantpol, nantpol)
+        #if antGains is not None: # Apply Gains
+        #    # TODO: this multiplication can be done outside the for loop
+        #    reducedCorrMatrix[tIdx] = np.multiply(sbVisGains, reducedCorrMatrix[tid, :, :]) # select out a single integration, shape (nantpol, nantpol)
 
         # correct the time relative to the EOF timestamp
         tOffset = (nints - tid - 1) * intTime # the timestamp in the filename is for the last integration
@@ -342,29 +355,57 @@ def lofarGenUVW(corrMatrix, ants, obs, sbs, ts):
             obs.epoch = ts[sbIdx, tIdx]
             obs.date = ts[sbIdx, tIdx]
 
-            # in order to accommodate multiple observations/subbands at different times/sidereal times all the positions need to be rotated relative to sidereal time 0
+            ## in order to accommodate multiple observations/subbands at different times/sidereal times all the positions need to be rotated relative to sidereal time 0
+            #LSTangle = obs.sidereal_time() # radians
+            #print 'LST:',  LSTangle, 'Dec:', obs.lat
+            #rotAngle = float(LSTangle) - float(obs.long) # adjust LST to that of the Observatory longitutude to make the LST that at Greenwich
+            #print float(LSTangle) * 180./np.pi, float(obs.long) * 180./np.pi, rotAngle * 180./np.pi
+            ## TODO: to be honest, the next two lines change the LST to make the images come out but i haven't worked out the coordinate transforms, so for now these work without justification
+            #rotAngle += np.pi
+            #rotAngle *= -1
+            ## Rotation matrix for antenna positions
+            #rotMatrix = np.array([[np.cos(rotAngle), -1.*np.sin(rotAngle), 0.],
+            #                      [np.sin(rotAngle), np.cos(rotAngle),     0.],
+            #                      [0.,               0.,                   1.]]) # rotate about the z-axis
+            #rotMatrix = np.array([[1., 0., 0.],
+            #                      [0., 1., 0.],
+            #                      [0., 0., 1.]]) # rotate about the z-axis
+
+            ## get antenna positions in ITRF (x,y,z) format and compute the (u,v,w) coordinates referenced to sidereal time 0, this works only for zenith snapshot xyz->uvw conversion
+            #xyz = np.dot(ants[:,0,:], rotMatrix)
+
+            #repxyz = np.repeat(xyz, nants, axis=0).reshape((nants, nants, 3))
+            #uvw[tIdx, :, :, sbIdx] = util.vectorize(repxyz - np.transpose(repxyz, (1, 0, 2)))
+
             LSTangle = obs.sidereal_time() # radians
-            print 'LST:',  LSTangle
-            rotAngle = float(LSTangle) - float(obs.long) # adjust LST to that of the Observatory longitutude to make the LST that at Greenwich
-            # to be honest, the next two lines change the LST to make the images come out but i haven't worked out the coordinate transforms, so for now these work without justification
-            rotAngle += np.pi
-            rotAngle *= -1
-            # Rotation matrix for antenna positions
-            rotMatrix = np.array([[np.cos(rotAngle), -1.*np.sin(rotAngle), 0.],
-                                  [np.sin(rotAngle), np.cos(rotAngle),     0.],
-                                  [0.,               0.,                   1.]]) # rotate about the z-axis
+            print 'LST:',  LSTangle, 'Dec:', obs.lat
 
-            # get antenna positions in ITRF (x,y,z) format and compute the (u,v,w) coordinates referenced to sidereal time 0, this works only for zenith snapshot xyz->uvw conversion
-            xyz = np.dot(ants[:,0,:], rotMatrix)
+            # Compute baselines in XYZ
+            antPosRep = np.repeat(ants[:,0,:], nants, axis=0).reshape((nants, nants, 3)) # ants is of the form [nants, npol, 3], assume pols are at the same position
+            xyz = util.vectorize(antPosRep - np.transpose(antPosRep, (1, 0, 2)))
 
-            repxyz = np.repeat(xyz, nants, axis=0).reshape((nants, nants, 3))
-            uvw[tIdx, :, :, sbIdx] = util.vectorize(repxyz - np.transpose(repxyz, (1, 0, 2)))
+            # Rotation matricies for XYZ -> UVW transform
+            #dec = float(obs.lat)
+            dec = float(np.pi/2.) # set the north pole to be dec 90, thus the dec rotation matrix below is not really needed
+            decRotMat = np.array([  [1., 0., 0.],
+                                    [0., np.sin(dec),     np.cos(dec)],
+                                    [0., -1.*np.cos(dec), np.sin(dec)]])
+            #ha = float(LSTangle) - float(obs.long) # Hour Angle
+            ha = float(LSTangle) - 0. # Hour Angle in reference to longitude/RA=0
+            haRotMat = np.array([   [np.sin(ha)    , np.cos(ha), 0.],
+                                    [-1.*np.cos(ha), np.sin(ha), 0.],
+                                    [0.,             0.,         1.]])
+            rotMatrix = np.dot(decRotMat, haRotMat)
+
+            uvw[tIdx, :, :, sbIdx] = np.dot(rotMatrix, xyz.T).T
 
             # split up polarizations, vectorize the correlation matrix, and drop the lower triangle
             vis[0, tIdx, :, sbIdx] = util.vectorize(corrMatrix[sbIdx, tIdx, 0::2, 0::2])
             vis[1, tIdx, :, sbIdx] = util.vectorize(corrMatrix[sbIdx, tIdx, 1::2, 0::2])
             vis[2, tIdx, :, sbIdx] = util.vectorize(corrMatrix[sbIdx, tIdx, 0::2, 1::2])
             vis[3, tIdx, :, sbIdx] = util.vectorize(corrMatrix[sbIdx, tIdx, 1::2, 1::2])
+
+    #exit()
 
     vis = np.reshape(vis, (vis.shape[0], vis.shape[1]*vis.shape[2], vis.shape[3])) 
     uvw = np.reshape(uvw, (uvw.shape[0]*uvw.shape[1], uvw.shape[2], uvw.shape[3])) 
@@ -599,16 +640,21 @@ def readMS(fn, sbs, column='DATA'):
 if __name__ == '__main__':
     print 'Running test cases...'
 
+    # LOFAR ACC
     fDict = parse('../examples/20150607_122433_acc_512x192x192.dat')
     print fDict
 
+    # Measurement Set
     fDict = parse('../examples/zen.2455819.26623.uvcRREM.MS')
     print fDict
 
+    # SE607 HBA XST
     fDict = parse('../examples/20150915_191137_rcu5_sb60_int10_dur10_elf0f39fe2034ea85fc02b3cc1544863053b328fd83291e880cd0bf3c3d3a50a164a3f3e0c070c73d073f4e43849c0e93b_xst.dat')
     print fDict
 
-    #TODO: KAIRA test
+    # KAIRA XST
+    fDict = parse('20160228_040005_xst.dat', fmt='KAIRA')
+    print fDict
 
     print '...Made it through without errors'
 

SWHT/swht.py
@@ -224,6 +224,10 @@ def iswhtVisibilities(blm, uvw, freqs):
     return vis
 
 #TODO: there is an issue about phase location, check this out
+# TODO: phase centre options:
+#           (RA, dec) : ( any, 90 deg)
+#           (RA, dec) : ( any,-90 deg)
+#           (RA, dec) : ( LST of first file, station latitude)
 def make2Dimage(coeffs, res, px=[64, 64], phs=[0., 0.]):
     """Make a flat image of a single hemisphere from SWHT image coefficients
     coeffs: SWHT brightness coefficients
@@ -243,11 +247,13 @@ def make2Dimage(coeffs, res, px=[64, 64], phs=[0., 0.]):
     #convert to polar positions
     r = np.sqrt(xx**2. + yy**2.)
     phi = np.arctan2(yy, xx)
-    #zero out undefined regions of the image where r>0
-    #numpy is super cunty and makes it fucking hard to do simple things, so the next few lines are bullshit that should only take 2 lines
-    idx = np.argwhere(r.flatten()>1)
+
+    # zero out undefined regions of the image where r>0
+    # overly tedious steps for something that should be much easier to do
     rflat = r.flatten()
     phiflat = phi.flatten()
+    maxRcond = r.flatten() > 1
+    idx = np.argwhere(maxRcond)
     rflat[idx] = 0.
     phiflat[idx] = 0.
     r = np.reshape(rflat, r.shape)
@@ -286,8 +292,7 @@ def make2Dimage(coeffs, res, px=[64, 64], phs=[0., 0.]):
 
     print 'Run time: %f s'%(time.time() - start_time)
 
-    #return theta0
-    return img
+    return np.ma.array(img, mask=maxRcond)
 
 #TODO: make3Dimage: masking
 def make3Dimage(coeffs, dim=[64, 64]):
@@ -311,6 +316,7 @@ def make3Dimage(coeffs, dim=[64, 64]):
     return img, phi, theta #flip theta and phi values
 
 #TODO: makeHEALPix: masking
+# TODO: there is a rotation issue, something like phi and theta need to be flipped but that doesn't seem to fix it
 def makeHEALPix(coeffs, nside=64):
     """Make a HEALPix map from SWHT image coefficients, comparable to healpy.alm2map()
     coeffs: SWHT brightness coefficients

scripts/swhtVisibilities.py
@@ -214,7 +214,8 @@ if __name__ == '__main__':
         res = fov/px[0] # pixel resolution
         print 'Generating 2D Hemisphere Image of size (%i, %i)'%(px[0], px[1])
         print 'Resolution(deg):', res*180./np.pi
-        img = SWHT.swht.make2Dimage(iImgCoeffs, res, px, phs=[0., float(obsLat)]) #TODO: 0 because the positions have already been rotated to the zenith RA of the first snapshot, if multiple snaphsots this needs to be reconsidered
+        img = SWHT.swht.make2Dimage(iImgCoeffs, res, px, phs=[0., 0.]) #TODO: 0 because the positions have already been rotated to the zenith RA of the first snapshot, if multiple snaphsots this needs to be reconsidered
+        #img = SWHT.swht.make2Dimage(iImgCoeffs, res, px, phs=[0., float(obsLat)]) #TODO: 0 because the positions have already been rotated to the zenith RA of the first snapshot, if multiple snaphsots this needs to be reconsidered
         fig, ax = SWHT.display.disp2D(img, dmode='abs', cmap='jet')
 
         # save complex image to pickle file
@@ -240,7 +241,8 @@ if __name__ == '__main__':
 
         # save complex image to HEALPix file
         print 'Writing image to file %s ...'%outFn,
-        hp.write_map(outFn, np.abs(m), coord='C') #TODO: should this be abs or real?
+        #hp.write_map(outFn, np.abs(m), coord='C') #TODO: should this be abs or real?
+        hp.write_map(outFn, m.real, coord='C') #TODO: should this be abs or real?
         print 'done'
     
     elif opts.imageMode.startswith('coeff'): # plot the complex coefficients