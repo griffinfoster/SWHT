fixed rotation issue I think, broke other stuff though

SWHT/fileio.py
@@ -350,12 +350,18 @@ def lofarGenUVW(corrMatrix, ants, obs, sbs, ts):
     nints = ts.shape[1]
     uvw = np.zeros((nints, ncorrs, 3, len(sbs)), dtype=float)
     vis = np.zeros((4, nints, ncorrs, len(sbs)), dtype=complex) # 4 polarizations: xx, xy, yx, yy
+
     for sbIdx, sb in enumerate(sbs):
         for tIdx in np.arange(nints):
-            obs.epoch = ts[sbIdx, tIdx]
-            obs.date = ts[sbIdx, tIdx]
+            #TODO: using a reference Obs emperically works, but I can't quite justify it yet
+            #TODO: using a reference Obs probably breaks the FT script, check if there is another roation needed
+            refObs = lofarObserver(0., -90., 0., ts[sbIdx, tIdx]) # create an observatory at (lat,long)=(0,-90) to get the sidereal time at the reference position, this is along the Y axis I believe
+            LSTangle = refObs.sidereal_time() # sidereal time at reference location, radians
 
-            LSTangle = obs.sidereal_time() # radians
+            #obs.epoch = ts[sbIdx, tIdx]
+            #obs.date = ts[sbIdx, tIdx]
+
+            #LSTangle = obs.sidereal_time() # radians
             print 'LST:',  LSTangle, 'Dec:', obs.lat
 
             # Compute baselines in XYZ
@@ -368,6 +374,7 @@ def lofarGenUVW(corrMatrix, ants, obs, sbs, ts):
                                     [0.,     np.sin(dec), np.cos(dec)],
                                     [0., -1.*np.cos(dec), np.sin(dec)]]) #rotate about x-axis
             ha = float(LSTangle) - 0. # Hour Angle in reference to longitude/RA=0
+            #ha = float(LSTangle) - 0. - (np.pi/2.) # Hour Angle in reference to longitude/RA=0, use if refObs at (0,0) 
             haRotMat = np.array([   [    np.sin(ha), np.cos(ha), 0.],
                                     [-1.*np.cos(ha), np.sin(ha), 0.],
                                     [0.,             0.,         1.]]) #rotate about z-axis
@@ -384,6 +391,7 @@ def lofarGenUVW(corrMatrix, ants, obs, sbs, ts):
     vis = np.reshape(vis, (vis.shape[0], vis.shape[1]*vis.shape[2], vis.shape[3])) 
     uvw = np.reshape(uvw, (uvw.shape[0]*uvw.shape[1], uvw.shape[2], uvw.shape[3])) 
 
+    #TODO: i don't think we need to return the LST angle
     return vis, uvw, LSTangle
 
 def readACC(fn, fDict, lofarStation, sbs, calTable=None):
@@ -553,6 +561,7 @@ def readKAIRAXST(fn, fDict, lofarStation, sbs, calTable=None, times='0'):
 
     return vis, uvw, freqs, [obsLat, obsLong, LSTangle]
 
+# TODO: add option to not apply rotation, useful for standard FT imaging
 def readMS(fn, sbs, column='DATA'):
     """Return the visibilites and UVW coordinates from a SE607 LOFAR XST format file
     fn: XST filename

SWHT/swht.py
@@ -53,7 +53,8 @@ def computeVislm(lmax, k, r, theta, phi, vis, lmin=0):
 
     returns: [lmax+1, 2*lmax+1, nfreq] array of coefficients, only partially filled, see for loops in this function
     """
-    #vis *= 2. #Treat the conjugate baslines as doubling the non-conjugate visibilities
+    #vis *= 2. #Treat the conjugate baselines as doubling the non-conjugate visibilities
+    #TODO: include conjugate baselines?
 
     nsbs = vis.shape[1]
     vislm = np.zeros((lmax+1, 2*lmax+1, nsbs), dtype=complex)

scripts/swhtVisibilities.py
@@ -200,7 +200,8 @@ if __name__ == '__main__':
         elif polMode=='XY': polVisComb = visComb[1]
         elif polMode=='YX': polVisComb = visComb[2]
         elif polMode=='YY': polVisComb = visComb[3]
-        imgCoeffs = SWHT.swht.swhtImageCoeffs(polVisComb, uvwComb, freqs, lmax=opts.lmax, lmin=opts.lmin)
+
+        imgCoeffs = SWHT.swht.swhtImageCoeffs(polVisComb, uvwComb, freqs, lmax=opts.lmax, lmin=opts.lmin) # perform SWHT
 
         # save image coefficients to file
         if opts.ocoeffs is None: outCoeffPklFn = 'tempCoeffs.pkl'
@@ -214,8 +215,6 @@ if __name__ == '__main__':
         if opts.imageMode.startswith('heal'): outFn = 'tempImage.hpx'
         else: outFn = 'tempImage.pkl'
     else: outFn = opts.of
-
-    print obsLat, obsLong, LSTangle
 
     #TODO: not doing the correct projection
     if opts.imageMode.startswith('2'): # Make a 2D hemispheric image
@@ -260,6 +259,6 @@ if __name__ == '__main__':
 
     if not (opts.savefig is None): plt.savefig(opts.savefig)
     if not opts.nodisplay:
-        if opts.imageMode.startswith('heal'): hp.mollview(np.abs(m))
+        if opts.imageMode.startswith('heal'): hp.mollview(m.real, coord='CG')
         plt.show()
 